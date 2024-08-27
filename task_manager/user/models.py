1b1519325a8629d00db652f23c443eaec6428d6d
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-27 15:44:42 +0200

    feat: create new user model

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+
+
+class User(AbstractUser):
+    """Authorization User Model."""
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.TextField(max_length=50, unique=True)
+    password = models.CharField(validators=[MinLengthValidator(6)])
+
+    def __str__(self):
+        return self.username
+
