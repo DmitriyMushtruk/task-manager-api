3e1d8b35c5edcb9704ab4992a752bc259280cc94
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-27 19:47:39 +0200

    feat: create UserRegistrationSerializer
    Uses with registration of new users
    Fields: first_name, last_name, username, password.
    'validate' method checks if password was repeated rightly.
    Create new user in database.

--- task_manager/user/serializers.py
+++ task_manager/user/serializers.py
@@ -1,11 +1,44 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from . import models
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """User model serializer."""
+    """General User model serializer."""
 
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "username", "id")
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Gets and validate data that required for creating new user in database.
+    """
+
+    first_name = serializers.CharField(max_length=30, required=True)
+    last_name = serializers.CharField(max_length=30, required=True)
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=models.User.objects.all()), ],
+        max_length=50,
+        required=True,
+    )
+    password = serializers.CharField(min_length=6, write_only=True, required=True)
+    password_confirmation = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "username", "password", "password_confirmation")
+
+    def validate(self, data):
+        """Checks if password was repeated rightly."""
+
+        if data["password"] != data["password_confirmation"]:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        """Create new user in database."""
+        validated_data.pop("password_confirmation")
+        return models.User.objects.create_user(**validated_data)
