ee744cb3c3359a11cc3717f79e9e0d8d4e19779b
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-27 18:24:04 +0200

    feat: create routes that allows get information about users
    Uses SimpleRouter class as generator of routes

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,1 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from . import views
+
+router = SimpleRouter()
+
+router.register(r"users", views.UserListViewSet, basename="users")
+
+app_name = "user"
+urlpatterns = [
+    path("", include(router.urls)),
+]
