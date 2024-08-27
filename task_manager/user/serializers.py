dfbf8796fc69a4fd9616a58b71e1ae34b73bbb80
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-27 18:19:05 +0200

    feat: create UserSerializer

--- task_manager/user/serializers.py
+++ task_manager/user/serializers.py
@@ -1,1 +1,11 @@
 from rest_framework import serializers
+
+from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """User model serializer."""
+
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "username", "id")
