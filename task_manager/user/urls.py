65ba568377229928b543943a7616a3014277eb80
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-27 22:02:36 +0200

    feat: add routes to get, refresh and verify tokens

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from . import views
 
@@ -10,5 +15,12 @@ router.register(r"register", views.RegisterUserViewSet, basename="registrations"
 
 app_name = "user"
 urlpatterns = [
+    # User routes
     path("", include(router.urls)),
+
+    # JWT tokens routes
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
