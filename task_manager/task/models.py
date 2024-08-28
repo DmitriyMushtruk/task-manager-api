5f32240ba0a20c65bc52628d079b37ebbe75921a
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-28 21:08:03 +0200

    feat: create Task model

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    TASK_STATUS_CHOICES = (
+        ('new', 'New'),
+        ('in progress', 'In Progress'),
+        ('completed', 'Completed'),
+    )
+
+    user_id = models.ForeignKey('user.User', to_field='id', on_delete=models.CASCADE, blank=False)
+    title = models.CharField(max_length=150, blank=False)
+    description = models.TextField(max_length=300, blank=True)
+    status = models.CharField(choices=TASK_STATUS_CHOICES, default='new')
+
+    def __str__(self):
+        return self.title
