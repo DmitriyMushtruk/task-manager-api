1c6148b2c0d520cb900f4d9753dc1bb8c87ddc70
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-29 13:13:14 +0200

    feat: include task.urls into general urls file

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -6,6 +6,7 @@ app_name = "rest_framework"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
-    path("api/", include("user.urls", "user_api"))
+    path("api/", include("user.urls", "user_api")),
+    path("api/", include("task.urls", "task_api"))
 
 ]
