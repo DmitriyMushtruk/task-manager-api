ebbf28a841df9396284ebec3bde3a7fab058c6fb
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-29 16:32:38 +0200

    test: add tests of update tasks
    Add:
    - test_update_task_success
    - test_update_task_without_title_field
    - test_update_task_with_non_exist_id
    - test_update_task_without_permissions

--- task_manager/tests/test_task.py
+++ task_manager/tests/test_task.py
@@ -140,3 +140,65 @@ def test_create_task_without_authorization(client, task_payload):
 
     assert response.status_code == 401
 
+
+"""Tests of PUT method to /api/task/ endpoint"""
+
+
+@pytest.mark.django_db
+def test_update_task_success(client, user_payload_access_token, task_payload, task_user):
+    """Checks successful updating of task."""
+
+    task_payload.pop("user_id")
+
+    response = client.put(
+        f"/api/tasks/{task_user.id}/",
+        task_payload,
+        HTTP_AUTHORIZATION=f"Bearer {user_payload_access_token}"
+    )
+
+    assert response.status_code == 200
+    assert response.data["title"] == task_payload["title"]
+    assert response.data["description"] == task_payload["description"]
+
+
+@pytest.mark.django_db
+def test_update_task_without_title_field(client, user_payload_access_token, task_payload, task_user):
+    """Checks updating task without title field."""
+
+    task_payload.pop("user_id")
+    task_payload.pop("title")
+
+    response = client.put(
+        f"/api/tasks/{task_user.id}/",
+        task_payload,
+        HTTP_AUTHORIZATION=f"Bearer {user_payload_access_token}"
+    )
+
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_update_task_with_non_exist_id(client, user_payload_access_token, task_payload, task_user):
+    """Checks updating task with non-exist id."""
+
+    response = client.put(
+        f"/api/tasks/{int(task_user.id * 25)}/",
+        task_payload,
+        HTTP_AUTHORIZATION=f"Bearer {user_payload_access_token}"
+    )
+
+    assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_task_without_permissions(client, user_payload_access_token, task_payload, task_second_user):
+    """Checks updating task without permissions."""
+
+    response = client.put(
+        f"/api/tasks/{task_second_user.id}/",
+        task_payload,
+        HTTP_AUTHORIZATION=f"Bearer {user_payload_access_token}"
+    )
+
+    assert response.status_code == 403
+
