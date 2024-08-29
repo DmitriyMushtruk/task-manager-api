e07a7630bf76e9ff93582b8088b0ef6ea39b7a06
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-29 13:10:46 +0200

    feat: create TaskVievSet
    Only authenticated users can get access to the tasks.
    Add get_queryset method for filtering records if query params were given.
    Otherwise returns list of all tasks.

--- task_manager/task/views.py
+++ task_manager/task/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from . import models
+
+from . import serializers
+
+from . import permissions
+
+
+class TaskViewSet(ModelViewSet):
+    serializer_class = serializers.TaskSerializer
+    permission_classes = [IsAuthenticated, permissions.IsOwnerOrReadOnly]
+    pagination_class = TaskListPagination
+
+    def get_queryset(self):
+        """
+        Returns list of all tasks if query params were not given.
+        Otherwise, returns queryset that was filtered using query params.
+        Allows to filter task list by fields: user_id, status.
+        """
+
+        queryset = models.Task.objects.all()
+
+        filter_params = {}
+        for param in ['user_id', 'status']:
+            value = self.request.query_params.get(param)
+            if value:
+                filter_params[param] = value
+
+        if filter_params:
+            queryset = queryset.filter(**filter_params)
+
+        return queryset
+
