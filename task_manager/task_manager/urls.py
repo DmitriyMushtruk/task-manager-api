c348ed97fc8042a38de90fa8445b50de095bbdae
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-29 18:30:52 +0200

    feat: add change default route for app to /login

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 app_name = "rest_framework"
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/login/', permanent=True)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
     path("api/", include("user.urls", "user_api")),
