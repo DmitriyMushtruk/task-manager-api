9c00b294c1aed26003d9f994dcd20edba144361d
Dmitriy Mushtruk <dima.mushtruk@gmail.com>
2024-08-29 16:28:06 +0200

    test: update fixtures in conftest file
    Add fixtures:
    - task_payload
    - task_user
    - task_user_completed
    - task_second_user

--- task_manager/tests/conftest.py
+++ task_manager/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from rest_framework.test import APIClient
 
 from user.models import User
+from task.models import Task
 
 
 @pytest.fixture
@@ -48,5 +49,53 @@ def second_user():
 def user_payload_access_token(user, client):
     request = client.post("/api/login/", dict(username="RealKnight", password="knight_pass", ))
     return request.data["access"]
+
+
+@pytest.fixture
+def task_payload(user):
+    return dict(
+        user_id=user,
+        title="Task title",
+        description="Task description",
+    )
+
+
+@pytest.fixture
+def task_user(user):
+    task = Task.objects.create(
+        user_id=user,
+        title="Do something",
+        description="Some description",
+    )
+    task.save()
+    return task
+
+
+@pytest.fixture
+def task_user_completed(user):
+    task = Task.objects.create(
+        user_id=user,
+        title="Something was already done",
+        description="Some description",
+        status="completed",
+    )
+    task.save()
+    return task
+
+
+@pytest.fixture
+def task_second_user(second_user):
+    task = Task.objects.create(
+        user_id=second_user,
+        title="Do something else",
+        description="Some different description",
+        status="in progress",
+    )
+    task.save()
+    return task
+
+
+
+
 
 
